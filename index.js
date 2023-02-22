50ee518d379362e033a3217234522bdfdf2edf61
Vitaliy <kacur@class.ztk.otg.ua>
2023-02-22 23:00:35 +0200

    1

---
+++ index.js
@@ -0,0 +1,5 @@
+const userName = prompt('Enter a name')
+const userLastName = prompt('Enter last name')
+const userAge = prompt('Enter age')
+
+console.log(`Мене звати ${userName} ${userLastName} та мені ${userAge} років !!!`)
